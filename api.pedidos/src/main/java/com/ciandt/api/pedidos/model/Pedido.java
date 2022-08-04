8bab5e6bedae52965a9eb43d4005b3bab6652ba8
Rebecabfv <re.bfviana@gmail.com>
2022-08-04 17:33:36 -0300

    [api.pedidos]: create model

---
+++ api.pedidos/src/main/java/com/ciandt/api/pedidos/model/Pedido.java
@@ -0,0 +1,31 @@
+package com.ciandt.api.pedidos.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_hora")
+    private LocalDateTime dataHora;
+
+    private Status status;
+
+    @OneToMany(mappedBy = "pedido")
+    Set<ItemPedido> itens;
+}
