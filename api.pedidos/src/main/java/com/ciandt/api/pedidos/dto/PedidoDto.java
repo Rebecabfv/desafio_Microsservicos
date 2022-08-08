e4a47c3452a03b22c6a4080dcb4841146824978e
Rebecabfv <re.bfviana@gmail.com>
2022-08-08 09:40:01 -0300

    [api.pedidos]: add dtos

--- api.pedidos/src/main/java/com/ciandt/api/pedidos/dto/PedidoDto.java
+++ api.pedidos/src/main/java/com/ciandt/api/pedidos/dto/PedidoDto.java
@@ -1,4 +1,12 @@
 package com.ciandt.api.pedidos.dto;
 
+import com.ciandt.api.pedidos.model.Status;
+
+import java.time.LocalDateTime;
+
 public class PedidoDto {
+
+    private LocalDateTime dataHora;
+
+    private Status status;
 }
